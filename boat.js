17d2962aafc8fe4c88862618971c982dde90d63d
Cristopher <cristcerpug@gmail.com>
2022-06-04 10:12:42 -0500

    a

---
+++ boat.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+var canvas = document.getElementById("canvas")
+
+var bote = new Bote();
+
+var hot = new Hot();
+
+var canvasContext = canvas.getContext('2d');
+
+window.onload = ()=>{
+	gameLoop;
+}
+
